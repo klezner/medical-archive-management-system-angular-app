57f2e6dc854f4f4dca2b04c96339e994fe4ce2b0
klezner <krzys3ek@gmail.com>
2021-08-19 13:56:35 +0200

    Implement getPatients method in patient service

--- src/app/patient/patient.service.ts
+++ src/app/patient/patient.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Patient} from "./patient";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+  private apiServerUrl = environment.localApiUrl;
+  private patientLocation = 'patient';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPatients(): Observable<HttpResponse<Patient[]>> {
+    return this.http.get<Patient[]>(`${this.apiServerUrl}/${this.patientLocation}`, {observe: 'response'});
+  }
 }
